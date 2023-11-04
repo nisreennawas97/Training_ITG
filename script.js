25fc1745ca0d8045824ba291f51f3c3335c43222
Beesan Khanfer <bkhanfar@itgsoftware.com>
2023-11-04 22:06:16 +0200

    enhance the code

--- script.js
+++ script.js
@@ -1,56 +1,54 @@
 const register_form = document.getElementById('register_form');
 const register_message = document.getElementById('register_message');
 
-register_form.addEventListener('submit', function(event)
-{
-    const student_fname = document.getElementById('student_fname');
-    const student_lname = document.getElementById('student_lname');
-    const student_email = document.getElementById('student_email');
-    const student_phone = document.getElementById('student_phone');
-    const student_major = document.getElementById('student_major');
+register_form.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-    const fnameError = document.getElementById('register_fnameError');
-    const lnameError = document.getElementById('register_lnameError');
-    const emailError = document.getElementById('register_emailError');
-    const trainError = document.getElementById('register_majorError');
-    const phoneError = document.getElementById('register_phoneError');
+    const validateInput = (element, regex, errorElement, errorMessage, regexMsg) => {
+        const value = element.value.trim();
+        if (!value) {
+            errorElement.innerHTML = errorMessage;
+            element.classList.add('invalid');
+            return false;
+        } else {
+            errorElement.innerHTML = '';
+            element.classList.remove('invalid');
+            if (regex && !regex.test(value)) {
+                errorElement.innerHTML = `Please enter a valid ${regexMsg}`;
+                element.classList.add('invalid');
+                return false;
+            }
+        }
+        return true;
+    };
+
+    const student_fname = document.querySelector('#student_fname');
+    const student_lname = document.querySelector('#student_lname');
+    const student_email = document.querySelector('#student_email');
+    const student_phone = document.querySelector('#student_phone');
+    const student_major = document.querySelector('#student_major');
+
+    const fnameError = document.querySelector('#register_fnameError');
+    const lnameError = document.querySelector('#register_lnameError');
+    const emailError = document.querySelector('#register_emailError');
+    const trainError = document.querySelector('#register_majorError');
+    const phoneError = document.querySelector('#register_phoneError');
 
     const emailRegex = /^[A-Za-z0-9._-]+@[A-Z0-9.-]+\.[a-z]{2,}$/i;
-    const phoneRegex = /^[0-9]{10}$/;
+    const phoneRegex = /^\d{10}$/;
 
-    fnameError.innerHTML = '';
-    lnameError.innerHTML = '';
-    emailError.innerHTML = '';
-    trainError.innerHTML = '';
-    phoneError.innerHTML = '';
+    const isValid = [
+        validateInput(student_fname, null, fnameError, 'First name is required', null),
+        validateInput(student_lname, null, lnameError, 'Last name is required', null),
+        validateInput(student_email, emailRegex, emailError, 'email address is required',  'Please enter a valid email address'),
+        validateInput(student_major, null, trainError, 'Major is required', null),
+        validateInput(student_phone, phoneRegex, phoneError, 'phone number is required',  'Please enter a valid 10-digit phone number')
+    ].every(Boolean);
 
-    if (student_fname.value.trim() === '') {
-        fnameError.innerHTML = 'First name is required';
-    }
-
-    if (student_lname.value.trim() === '') {
-        lnameError.innerHTML = 'Last name is required';
-    }
-
-    if (!student_email.value.match(emailRegex)) {
-        emailError.innerHTML = 'Please enter a valid email address';
-    }
-
-    if (student_major.value.trim() === '') {
-        trainError.innerHTML = 'Major is required';
-    }
-
-    if (!student_phone.value.match(phoneRegex)) {
-        phoneError.innerHTML = 'Please enter a valid 10-digit phone number';
-    }
-
-    if (!(fnameError.innerHTML === "" && lnameError.innerHTML === "" && emailError.innerHTML === "" 
-    && trainError.innerHTML === "" && phoneError.innerHTML === "")) {
-        event.preventDefault();
-    }
-    else{
-        event.preventDefault();
-        register_message.style.display = "block";
+    if (isValid) {
+        register_form.reset();
+        register_message.style.display = 'block';
+    } else {
+        register_message.style.display = 'none';
     }
 });
-
